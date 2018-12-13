4a3688ca2ff7c77233fd98a7c2938ad2cfd8e807
mablic <maihe88@gmail.com>
2018-12-12 22:10:22 -0600

    change py function & add sample graph

--- model/base.py
+++ model/base.py
@@ -8,42 +8,55 @@ from mpl_finance import candlestick_ohlc
 from datetime import datetime as dt
 
 
+COLOR = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+
+
 def get_data(ticket, startdate, enddate, source='yahoo'):
-	df=web.DataReader(ticket, source, startdate, enddate)
-	df.to_csv(os.getcwd() + '\\%sdata.csv' % ticket)
+    df=web.DataReader(ticket, source, startdate, enddate)
+    df.to_csv(os.getcwd() + '\\%sdata.csv' % ticket)
 
-def read_data(ticket):
-	df = pd.read_csv(os.getcwd() + '\\%sdata.csv' %ticket, index_col=None)
-	return df
 
-def graph_data(df, mv1, mv2):
-	df['Date'] = pd.datetools.to_datetime(df['Date'])
-	df['Date'] = df['Date'].map(mdates.date2num)
+def read_data(ticker):
+    df = pd.read_csv(os.getcwd() + '\\%sdata.csv' %ticker, index_col=None)
+    return df
 
-	df_ohlc = df[mv2:]
-	df_ohlc = df_ohlc[['Date', 'Open', 'High', 'Low', 'Close', 'Volume']]
-	df_ohlc[str(mv1) + 'ma'] = df['Close'].rolling(mv1).mean()
-	df_ohlc[str(mv2) + 'ma'] = df['Close'].rolling(mv2).mean()
 
-	fig = plt.figure()
-	ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
-	ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
-	ax1.xaxis_date()
-	candlestick_ohlc(ax1, df_ohlc.values, width=0.5, colorup='#77d879', colordown='#db3f3f')
-	ax1.plot(df_ohlc.Date, df_ohlc[str(mv1) + 'ma'], color='b', label=str(mv1) + ' days moving average.')
-	ax1.plot(df_ohlc.Date, df_ohlc[str(mv2) + 'ma'], color='r', label=str(mv2) + ' days moving average.')
-	ax2.bar(df_ohlc.Date, df_ohlc.Volume)
-	plt.setp(ax1.get_xticklabels(), visible=False)
+def graph_data(ticker, *mvs):
+    try:
+        df_ohlc = read_data(ticker)
+    except ValueError:
+        print('No csv file for ticker %s.' % ticker)
 
-	ax1.set_title('%d days moving average vs %d days moving average.' % (mv1, mv2) )
-	ax1.legend(loc='best')
-	for label in ax2.xaxis.get_ticklabels():
-		label.set_rotation(45)
+    max_date = max(mvs)
+    df_ohlc['Date'] = pd.to_datetime(df_ohlc['Date'])
+    df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
 
-	plt.show()
+    df_ohlc = df_ohlc[['Date', 'Open', 'High', 'Low', 'Close', 'Volume']]
+    for mv in mvs:
+        df_ohlc[str(mv) + 'ma'] = df_ohlc['Close'].rolling(mv).mean()
+
+    df_ohlc.drop([i for i in range(max_date)], inplace=True)
+    fig = plt.figure()
+    ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
+    ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
+    ax1.xaxis_date()
+    candlestick_ohlc(ax1, df_ohlc.values, width=0.5, colorup='#77d879', colordown='#db3f3f')
+
+    for i in range(len(mvs)):
+        temp_color = COLOR[i % len(COLOR)]
+        ax1.plot(df_ohlc.Date, df_ohlc[str(mvs[i]) + 'ma'], color=temp_color, label=str(mvs[i]) + ' days moving average.')
+    #ax1.plot(df_ohlc.Date, df_ohlc[str(mv2) + 'ma'], color='r', label=str(mv2) + ' days moving average.')
+    ax2.bar(df_ohlc.Date, df_ohlc.Volume)
+    plt.setp(ax1.get_xticklabels(), visible=False)
+
+    ax1.set_title('%s historical stock prices' % ticker)
+    ax1.legend(loc='best')
+    for label in ax2.xaxis.get_ticklabels():
+        label.set_rotation(45)
+
+    plt.show()
+
 
 if __name__ == '__main__':
-	#get_data('spy', '2017-1-1', '2018-11-30')
-
-	df = read_data('spy')
-	graph_data(df, 10, 20)
+    #get_data('spy', '2017-1-1', '2018-11-30')
+    graph_data('spy', 10, 20, 100)
